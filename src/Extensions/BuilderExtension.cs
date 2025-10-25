bc1da0f68430d1f62d333e47185ff13b9166b014
Guilherme Agunzo <g.agunzo@gmail.com>
2025-10-25 13:03:29 -0300

    feat: register product handler in service collection

--- src/Extensions/BuilderExtension.cs
+++ src/Extensions/BuilderExtension.cs
@@ -7,5 +7,6 @@ public static class BuilderExtension
   public static void AddServices(this WebApplicationBuilder builder)
   {
     builder.Services.AddTransient<Abstractions.Handlers.Customers.IHandler, Handlers.Customers.Handler>();
+    builder.Services.AddTransient<Abstractions.Handlers.Products.IHandler, Handlers.Products.Handler>();
   }
 }
