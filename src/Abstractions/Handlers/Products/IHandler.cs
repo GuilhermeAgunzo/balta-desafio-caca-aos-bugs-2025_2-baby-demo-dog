340f38195a3a4de8b78f4a4e7894a77da5265b86
Guilherme Agunzo <g.agunzo@gmail.com>
2025-10-25 12:40:39 -0300

    feat: add IHandler interface for product operations

---
+++ src/Abstractions/Handlers/Products/IHandler.cs
@@ -0,0 +1,10 @@
+namespace BugStore.Abstractions.Handlers.Products;
+
+public interface IHandler
+{
+  Task<Responses.Products.Get> GetProductsAsync(Requests.Products.Get request, CancellationToken cancellationToken);
+  Task<Responses.Products.GetById> GetProductByIdAsync(Requests.Products.GetById request, CancellationToken cancellationToken);
+  Task<Responses.Products.Create> CreateProductAsync(Requests.Products.Create request, CancellationToken cancellationToken);
+  Task<Responses.Products.Update> UpdateProductAsync(Requests.Products.Update request, CancellationToken cancellationToken);
+  Task<Responses.Products.Delete> DeleteProductAsync(Requests.Products.Delete request, CancellationToken cancellationToken);
+}
